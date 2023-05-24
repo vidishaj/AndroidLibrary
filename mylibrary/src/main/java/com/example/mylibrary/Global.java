6e2a615d432d2f170c2060b72121cc6d69b6917f
vidishaj <vidisha.jhanwar@gmail.com>
2023-05-24 14:09:38 +0530

    Login page added

---
+++ mylibrary/src/main/java/com/example/mylibrary/Global.java
@@ -0,0 +1,6 @@
+package com.example.mylibrary;
+
+public class Global {
+    public static String v1EndPoint = "";
+    public static String checkUser = v1EndPoint + "/v1/signup/checkUser";
+}
--- mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.java
+++ mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.java
@@ -2,9 +2,13 @@ package com.example.mylibrary;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.Toast;
 
 public class ToasterMessage {
+
+    static SharedPreferences sharedPreferences;
+    static SharedPreferences.Editor editor;
 
     public static void s(Context c, String message){
 
@@ -17,7 +21,14 @@ public class ToasterMessage {
         Intent profileIntent = new Intent();
         profileIntent.setClass(c, MainActivity.class);
         c.startActivity(profileIntent);
+    }
 
+    public static void initData(Context c ,String tenant,String v1Url,String v2Url,String bucketPoolIdNew,String bucketPoolIdOld,String sharedPreference,String LoginStatus, Class landingClass){
+
+        sharedPreferences = c.getSharedPreferences(sharedPreference, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        Global.v1EndPoint =v1Url;
 
     }
 }
