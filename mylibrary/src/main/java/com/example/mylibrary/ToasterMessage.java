b9ca7d5a08aef255e8ecee9e165014cf52d861a7
vidishaj <vidisha.jhanwar@gmail.com>
2023-05-24 10:43:40 +0530

    singmup lib in progress

--- mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.java
+++ mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.java
@@ -1,6 +1,7 @@
 package com.example.mylibrary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class ToasterMessage {
@@ -8,6 +9,15 @@ public class ToasterMessage {
     public static void s(Context c, String message){
 
         Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public static void launchActivity(Context c, Class c1){
+
+        Intent profileIntent = new Intent();
+        profileIntent.setClass(c, c1);
+        c.startActivity(profileIntent);
+
 
     }
 }
